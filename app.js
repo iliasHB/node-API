481c0de770e1352c130e8730b46b0c5c34822d0e
hsoliu <abeeb.ilias@gmail.com>
2022-09-12 08:59:18 +0100

    checkout payment page added

--- app.js
+++ app.js
@@ -50,13 +50,24 @@ app.get('/blogs',(req, res) => {
 
 //create a blog ans sae it in the database
 app.post('/blog', (req, res) => {
-    console.log(req.body);
-    const blog = new Blog(req.body);
-    blog.save().then((result) => {
-        res.send('Blog saved successfully')
-    }).catch((error) => {
-        res.status(404).send('Blog failed to save')
-    })
+    //console.log(req.body);
+    //const blog = new Blog(req.body);
+    let blog = new Blog ({
+        title,
+        snippet,
+        body
+    });
+    console.log(blog);
+    // blog.save().then((result) => {
+    //     //res.send('Blog saved successfully')
+    //     res.json({
+    //         status: 'ok', 
+    //         message: 'Blog saved successfully',
+    //         data: result
+    //     })
+    // }).catch((error) => {
+    //     res.status(404).send('Blog failed to save')
+    // })
 });
 
 //Search blog by id to view more detail about the blog
@@ -76,25 +87,31 @@ app.delete('blog/:id', (req, res) => {
     console.log(id);
     Blog.findByIdAndDelete(id).then((result) => {
         //res.send('Bolg was deleted successfully')
-        res.json({redirect: '/blog'})
+        res.json({redirect: '/blogs'})
         .catch((error) => {
             res.status(404).send('Blog does not exist');
         })
     })
 })
 
-app.get('/about', (req, res) => {
-    res.send('Welcome to about page');
+app.get('/checkout', (req, res) => {
+    res.sendFile('./views/remita_checkout.html', {root: __dirname});
+    //res.setHeader('contect-type', 'text/html');
+    //res.write('<html><head><title>Remita Checkout Sample</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script><style type="text/css">.button {background-color: #1CA78B;  border: none;  color: white;  padding: 15px 32px;  text-align: center;  text-decoration: none;  display: inline-block;  font-size: 16px;  margin: 4px 2px;  cursor: pointer;  border-radius: 4px;}input {max-width: 30%;}</style></head><body><h2>Remita Checkout Demo</h2><p>Try out our Payment Gateway</p><form onsubmit="makePayment()" id="payment-form"><input type="button" onclick="makePayment()" value="Submit" button class="button"/></form><script type="text/javascript" src="https://remitademo.net/payment/v1/remita-pay-inline.bundle.js"></script></body>')
+    makePayment();
+    console.log('success'); 
+});
+
+app.get('/checkout-RRR', (req, res) => {
+    res.sendFile('./views/remita_RRR.html', {root: __dirname});
 });
 
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
 });
 
-app.get('/create-blog', (req, res) => {
-    res.send('Welcome to about page');
-});
 
 app.use((req, res) => { 
     res.status(404).send('404 - page not found');
 });
+
