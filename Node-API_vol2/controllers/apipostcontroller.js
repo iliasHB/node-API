eb9d6a7f488317391cfed98cec59bbe15e7f7835
hsoliu <abeeb.ilias@gmail.com>
2022-10-01 23:56:04 +0100

    post API modified

--- Node-API_vol2/controllers/apipostcontroller.js
+++ Node-API_vol2/controllers/apipostcontroller.js
@@ -5,47 +5,45 @@ const userPost = require('../models/userpost');
 
 
 const api_userPost = async (req, res) => {
-    const {userId, authorId, postId, username, postBody} = req.body;
+    const {userId, authorId, postId, postBody} = req.body;
     //let _id = userId;
     let rd = Math.floor(Math.random() * (1000 - 0 + 1000)) + 0;
     let post_Id = Date().substring(22, 25).trim()+rd; //dateFormat("hh:mm:ss");
-    //console.log(">>>>>>>>>>>>>>>>ramdom: "+rd);
-    //console.log(">>>>>>>>>>>>>>>> date: "+dt);
-    //mongoose.Types.ObjectId.isValid(userId);
-    let user = await Register.find({username: username, _id:userId})
+
+    let user = await Register.find({_id:userId})
  console.log(">>>>>>>>>>>>>>>> user: "+user);
     if (!user) {
         return res.json({
             status: "Failed",
             message: "User does not exist"
         })
-    } else if(userId !== authorId) {
+    } else if (userId !== authorId) {
         return res.json({
             status: "Failed",
-            message: "You don't have permission to post with someone else userId"
-        })
-    } else {
-        user = new userPost({
-            authorId,
-            userId,
-            postId: post_Id,
-            username,
-            postBody
+            message: "You don't have permission to post"
         })
     }
+    
+    let post = new userPost({
+        authorId,
+        userId,
+        postId: post_Id,
+        postBody
+    })
     //var token = jwt.sign({ id: user.id }, "password")
-       console.log(user)
-      if (userId == '' || username == '' || postBody == '') {
+       console.log(">>>>>>>>>>>>User post: "+post)
+      if (userId == '' || postBody == '' || authorId == '') {
           return res.status(404).json({
               status: "Failed",
               message: "User post failed, No data supplied by the user",
           })
       } else {
-          await user.save().then((result) => {
+          await post.save().then((result) => {
+            console.log(">>>>>>>>>>>>Username: "+user);
               return res.json({
                   status: "sucess",
                   message: "The Post is successful",
-                  data: {"username": result.username, "body": result.postBody, "date": result.createdAt}
+                  data: {"userInfo": user, "body": result.postBody, "date": result.createdAt}
               })
           }).catch((error) => {
               return res.status(404).json({
